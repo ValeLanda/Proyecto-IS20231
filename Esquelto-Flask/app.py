def2717fdd79abb6ce2f92f545ba2a12d1f866b8
ValeLanda <valeriaa.landaa@ciencias.unam.mx>
2022-11-28 15:29:44 -0600

    Más funcionalidades

---
+++ Esquelto-Flask/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, redirect, url_for
+from alchemyClasses.usuario import db
+from controllers.auth import bp
+from controllers.login import loginBlueprint
+from controllers.logout import logoutBlueprint
+from controllers.reservations import view_reservations
+
+app = Flask(__name__, instance_relative_config=True)
+app.register_blueprint(bp)
+app.register_blueprint(loginBlueprint)
+app.register_blueprint(logoutBlueprint)
+app.register_blueprint(view_reservations)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:Altair97!@localhost:3306/prueba"
+app.config.from_mapping(
+    SECRET_KEY = 'dev'
+)
+db.init_app(app)
+
+@app.route('/', methods=['GET','POST'])
+def hello_world():
+    return redirect(url_for('login.login'))
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run()
+
+
